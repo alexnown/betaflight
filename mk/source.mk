be7536e6056ef0b81c9d64bac95536493eb81e6e
Oleksandr Sierovoi <alexander.serovoy@gmail.com>
2024-08-11 20:44:00 +0300

    added link

--- mk/source.mk
+++ mk/source.mk
@@ -63,6 +63,7 @@ COMMON_SRC = \
             io/transponder_ir.c \
             io/usb_cdc_hid.c \
             io/usb_msc.c \
+            io/spam.c \
             msp/msp.c \
             msp/msp_box.c \
             msp/msp_build_info.c \
